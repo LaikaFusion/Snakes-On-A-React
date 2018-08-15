58ee5fe3acf3a22857db245233581d3cab9a80bb
LaikaFusion <newrad4@gmail.com>
2018-08-15 10:20:30 -0400

    styling and misc

--- snake/src/App.js
+++ snake/src/App.js
@@ -9,13 +9,15 @@ class App extends Component {
       pixels: Array(252).fill({ on: false }),
       direction: "right",
       snake: [136, 135, 134, 133],
-      food: 0,
+      food: -1,
       ylinesize: 21,
       xlinesize: 12,
       removePixel: 0,
       nextPixel: 0,
       intervalId: 0,
-      intervalAmount: 200
+      intervalAmount: 200,
+      gameOver: false,
+      gameStarted: false
     };
   }
   componentWillMount() {
@@ -62,35 +64,44 @@ class App extends Component {
         break;
     }
   };
-  newGame = () => {
-    let gameStart = this.state.pixels.slice();
+  newGame = (e) => {
+    this.setState({
+      gameOver: false,
+      direction: "right",
+      snake: [136, 135, 134, 133],
+      intervalAmount: 200,
+      pixels: Array(252).fill({ on: false }),
+      gameStarted: true
+    },()=>{
 
-    this.state.snake.forEach(element => {
-      gameStart[element] = { on: true };
-    });
-    gameStart[this.state.food] = { on: true };
-    this.setState(
-      {
-        pixels: gameStart
-      },
-      () => {
-        this.gameRunTime(200);
-      }
-    );
+      let gameStart = this.state.pixels.slice();
+      this.state.snake.forEach(element => {
+        gameStart[element] = { on: true };
+      });
+      gameStart[this.state.food] = { on: true };
+      this.setState(
+        {
+          pixels: gameStart
+        },
+        () => {
+          this.gameRunTime(200);
+        }
+      );
+    })
+
   };
   randomFoodSpot = () => {
     const foodLocation = Math.floor(Math.random() * this.state.pixels.length);
-    if (this.state.snake.indexOf(foodLocation) !== -1) {
+    if (this.state.snake.indexOf(foodLocation) !== -1 ||foodLocation=== 0||foodLocation===20 ||foodLocation=== 231||foodLocation===251) {
       this.randomFoodSpot();
     } else {
-      this.goFaster();
+      
       this.setState({
         food: foodLocation
       });
     }
-    
   };
-  gameRunTime = (int) => {
+  gameRunTime = int => {
     const interval = setInterval(() => {
       this.animate();
     }, int);
@@ -114,8 +125,8 @@ class App extends Component {
         } else {
           snakeMod.unshift(nextPixelRight);
           nextPixelRight = snakeMod[0] + 1;
-
           this.randomFoodSpot();
+          this.goFaster();
         }
 
         if (nextPixelRight % 21 === 0) {
@@ -143,11 +154,12 @@ class App extends Component {
           toRemove = snakeMod.pop();
         } else {
           this.randomFoodSpot();
+          this.goFaster();
           snakeMod.unshift(nextPixelLeft);
 
           nextPixelLeft = snakeMod[0] - 1;
         }
-        
+
         if ((nextPixelLeft + 1) % 21 === 0) {
           this.gameOver();
           break;
@@ -173,11 +185,12 @@ class App extends Component {
           toRemove = snakeMod.pop();
         } else {
           this.randomFoodSpot();
+          this.goFaster();
           snakeMod.unshift(nextPixelUp);
 
           nextPixelUp = snakeMod[0] - this.state.ylinesize;
         }
-      
+
         if (nextPixelUp < 0) {
           this.gameOver();
           break;
@@ -203,12 +216,13 @@ class App extends Component {
           toRemove = snakeMod.pop();
         } else {
           this.randomFoodSpot();
+          this.goFaster();
           snakeMod.unshift(nextPixelDown);
 
           nextPixelDown = snakeMod[0] + this.state.ylinesize;
         }
-       
-        if (nextPixelDown > this.state.pixels.length) {
+
+        if (nextPixelDown >= this.state.pixels.length) {
           this.gameOver();
           break;
         }
@@ -240,26 +254,30 @@ class App extends Component {
       pixels: pixelsUpdate
     });
   };
-  goFaster = () =>{
-    const newAmount = (this.state.intervalAmount) - 5;
-    if(newAmount < 50){
-      return
+  goFaster = () => {
+    const newAmount = this.state.intervalAmount - 5;
+    if (newAmount < 50) {
+      return;
     }
     clearInterval(this.state.intervalID);
-    
-  
-    this.gameRunTime(newAmount)
-  }
+
+    this.gameRunTime(newAmount);
+  };
   gameOver = () => {
     clearInterval(this.state.intervalID);
-    console.log("Over");
+    this.setState({
+      gameOver: true,
+      gameStarted: false
+    })
   };
   render() {
     return (
       <div className="App">
+      <div className='screenContainer'>
         <Screen pixels={this.state.pixels} />
-        <button onClick={this.newGame}> Start </button>
-        <button onClick={this.gameOver}> Test </button>
+        <div className="score"> Score: {(this.state.snake.length -4)/2}</div>
+        <div className='buttonContainer'><button className={this.state.gameStarted  ? 'StartButton hidden':'StartButton' } onClick={this.newGame}>{this.state.gameOver?"Game Over Again?":"Start!"}</button></div>
+      </div>
       </div>
     );
   }
--- snake/src/components/Screen.jsx
+++ snake/src/components/Screen.jsx
@@ -10,7 +10,12 @@ class Screen extends Component {
     return (
       <div className='screen'>
         {this.props.pixels.map((e,i)=>{
-            return <div key ={i} id= {i} className={e.on? 'pixel on':'pixel'} />
+            return <div key ={i} id= {i} className={e.on? 'pixel ':'pixel'} >
+            <div className={e.on? ' on':''} />
+            <div className={e.on? ' on':''} />
+            <div className={e.on? ' on':''}/>
+            <div className={e.on? ' on':''}/>
+            </div>
         })}
         
       </div>
